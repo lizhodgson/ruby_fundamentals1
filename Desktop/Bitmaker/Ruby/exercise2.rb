32225e15e78858a6905d78c73a64636c52262352
lizhodgson <lizhodgson@gmail.com>
2015-01-20 14:37:26 -0500

    used interpolation

--- Desktop/Bitmaker/Ruby/exercise2.rb
+++ Desktop/Bitmaker/Ruby/exercise2.rb
@@ -7,3 +7,9 @@ string = "Liz"
 integer = 20.to_s
 puts string + integer
 
+# "my string".length
+# my_method(arguments go here)
+
+result = (45628 * 7839).to_s
+puts("The total of 45628 times 7839 is = " + result)
+puts("The total of 45628 times 7839 is #{result}")
