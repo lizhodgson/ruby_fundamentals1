457f53a339f8afd2ba2c9a53e25442db963e0b5f
lizhodgson <lizhodgson@gmail.com>
2015-01-20 16:28:59 -0500

    used a loop to solve fizzbuzz problem.

---
+++ Desktop/Bitmaker/Ruby/exercise4.rb
@@ -0,0 +1,14 @@
+1.upto(100) do |i|
+	if i % 5 == 0 and i % 3 == 0
+		puts "bitmaker labs"
+	elsif i % 5 == 0
+		puts "labs"
+	elsif i % 3 == 0
+		puts "bitmaker"
+	else
+		puts(i)
+	end
+end
+
+			
+			
