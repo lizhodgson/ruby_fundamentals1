f68dfc885f2fcace75442e04821277b61f4639b2
lizhodgson <lizhodgson@gmail.com>
2015-01-20 15:13:48 -0500

    I wrote code that expressed year of birth by asking age

--- Desktop/Bitmaker/Ruby/exercise3.rb
+++ Desktop/Bitmaker/Ruby/exercise3.rb
@@ -1,1 +1,9 @@
 puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}."
+
+puts "How old are you?"
+age = gets.chomp
+age = age.to_i
+year = 2015 - age
+puts "You were born in the year #{year}"
